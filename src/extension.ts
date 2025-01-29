f0ca1bc8a1822d82c7bfd6b4f1437958f5f1ac30
huangjien <huangjien@gmail.com>
2025-01-29 11:17:49 +0000

    minor

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,5 @@
 "use strict";
 import * as vscode from "vscode";
-import { setupL10N } from "./Functions";
 import { register2VSCode } from "./commands";
 
 export function activate(context: vscode.ExtensionContext) {
